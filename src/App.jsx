966199662174e95e693de3ce6eb54b708a43eed0
XavierMurillo <xaviermurillosanchez@gmail.com>
2024-04-21 13:04:29 -0400

    Added main view component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import MainView from "./views/mainView";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Footer />
+      <MainView />
     </>
   );
 }
