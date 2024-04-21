1931dacc61803a4a0b787767ecdec63ed70574bf
XavierMurillo <xaviermurillosanchez@gmail.com>
2024-04-21 12:15:57 -0400

    worked on navbar style

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,20 +3,22 @@ import { FaSearch } from "react-icons/fa";
 import ButtonComponent from "./ButtonComponent";
 
 export const SearchBar = () => {
-    const [input, setInput] = useState("");
+  const [input, setInput] = useState("");
   return (
     <>
-        <form className="form-group search-container">
-            <input 
-                className="form-control search-bar" 
-                placeholder="¿Qué estás buscando?"
-                value={input}
-                onChange={(e)=>{setInput(e.target.value)}}
-            />
-            <ButtonComponent buttonBody={<FaSearch />} />
-        </form>
+      <form className="search-container">
+        <input
+          className="form-control search-bar"
+          placeholder="¿Qué estás buscando?"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+        <ButtonComponent buttonBody={<FaSearch />} />
+      </form>
     </>
-  )
-}
+  );
+};
 
 export default SearchBar;
