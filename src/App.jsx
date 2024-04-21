c2d711310266abac94a46bb4b6534fb38ff4a884
Maur1998 <mmurillosanchez1998@gmail.com>
2024-04-21 13:22:40 -0400

    adding product view

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import ProductView from "./views/ProductView";
 
 function App() {
   return (
     <>
       <Navbar />
+      <ProductView />
       <Footer />
     </>
   );
