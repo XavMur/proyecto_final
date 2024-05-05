97a7bcaa8d2e0c8ac68d60e1b1713148bda423e5
XavierMurillo <xaviermurillosanchez@gmail.com>
2024-05-05 01:40:50 -0400

    Added routing logic to button component

--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -1,13 +1,30 @@
 import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
-const ButtonComponent = ({ buttonBody }) => {
+const ButtonComponent = ({ buttonBody, path = "" }) => {
   const [buttonValue, setButtonValue] = useState("");
+  const [changePath, setChangePath] = useState(false);
+  const navigate = useNavigate();
+  const changeRoute = (path) => {
+    navigate(path);
+  };
+
   useEffect(() => {
     setButtonValue(buttonBody);
   }, [buttonBody]);
+
+  useEffect(() => {
+    changeRoute(path);
+    setChangePath(false);
+  }, [changePath]);
+
   return (
-    <Button style={{ marginRight: "10px" }} variant="outline-info">
+    <Button
+      style={{ marginRight: "10px" }}
+      variant="outline-info"
+      onClick={() => setChangePath(true)}
+    >
       {buttonValue}
     </Button>
   );
