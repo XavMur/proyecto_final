e00ec111836e7262b9a96c9a9f7b7b17c0246dd4
XavierMurillo <xaviermurillosanchez@gmail.com>
2024-05-04 23:33:01 -0400

    Fixed main item view

--- src/components/MainViewItemComponent.jsx
+++ src/components/MainViewItemComponent.jsx
@@ -2,12 +2,18 @@ import React from "react";
 
 const MainViewItemComponent = () => {
   return (
-    <div className="main-grid-item">
+    <div className="col-12 col-md-4 col-lg-3  main-item">
       <img
         src="https://i.ebayimg.com/images/g/pHwAAOSw7Cdh5uD5/s-l1200.webp"
-        style={{ maxHeight: "80%", width: "100%" }}
+        style={{
+          marginLeft: "30px",
+          marginTop: "10px",
+          maxHeight: "80%",
+          width: "90%",
+          borderRadius: "15px",
+        }}
       />
-      <strong style={{ textAlign: "center" }}>Titulo del producto</strong>
+      <strong>Titulo del producto</strong>
       <p>Precio: 100000$</p>
     </div>
   );
