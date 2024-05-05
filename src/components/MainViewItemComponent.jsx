c787f63715f84aab3e09cb9c435f78698193b779
XavierMurillo <xaviermurillosanchez@gmail.com>
2024-05-05 00:51:21 -0400

    Updated main item view

--- src/components/MainViewItemComponent.jsx
+++ src/components/MainViewItemComponent.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const MainViewItemComponent = ({ image, title, price }) => {
+  const navigate = useNavigate();
+  const changeRoute = () => {
+    navigate("/product");
+  };
   return (
     <div className="col-12 col-md-4 col-lg-3  main-item">
-      <img
-        src={image}
-        style={{
-          marginLeft: "30px",
-          marginTop: "10px",
-          maxHeight: "80%",
-          width: "90%",
-          borderRadius: "15px",
-        }}
-      />
-      <strong>{title}</strong>
-      <p>{price}$</p>
+      <div className="main-item-container">
+        <img
+          onClick={changeRoute}
+          src={image}
+          style={{
+            maxHeight: "80%",
+            width: "90%",
+            borderRadius: "15px",
+          }}
+        />
+        <strong>{title}</strong>
+        <p>{price}$</p>
+      </div>
     </div>
   );
 };
