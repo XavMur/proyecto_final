61c0d18fe1d4e036e0c8dd07383062e4b9f72a88
Maur1998 <mmurillosanchez1998@gmail.com>
2024-05-12 18:43:20 -0400

    ase

--- src/views/GridView.jsx
+++ src/views/GridView.jsx
@@ -1,23 +1,53 @@
+import { useState } from "react";
 import MainViewItemComponent from "../components/MainViewItemComponent";
 
-export const GridView = ({products}) => {
+export const GridView = ({products, categories}) => {
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleCheckboxChange = () => {
+      setIsChecked(!isChecked);
+    };
   return (
-    <div className="container">
-        <div className="row" style={{marginTop:"1rem "}}>
+    <div className="grid-view">
+        <div className="sidebar">
+            <h3>Categorias</h3>
             {
-                products ?
-                products.map(product=>(
-                    <div key={product.id} className="col-12 col-md-4 col-lg-3">
-                        <MainViewItemComponent
-                            image={product.img}
-                            title={product.title}
-                            price={product.price}
+                categories 
+                ?categories.map(categ=>(
+                    <div key={categ.id} className="form-check">
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            value={isChecked}
+                            id="flexCheckDefault"
+                            onChange={handleCheckboxChange}
                         />
+                        <label className="form-check-label">
+                            {categ.nombre}
+                        </label>
                     </div>
                 ))
                 :
                 <div></div>
             }
+        </div>
+        <div className="container grid-container">
+            <div className="row" style={{marginTop:"1rem "}}>
+                {
+                    products ?
+                    products.map(product=>(
+                        <div key={product.id} className="col-12 col-md-4 col-lg-3">
+                            <MainViewItemComponent
+                                image={product.img}
+                                title={product.title}
+                                price={product.price}
+                            />
+                        </div>
+                    ))
+                    :
+                    <div></div>
+                }
+            </div>
         </div>
     </div>
   )
